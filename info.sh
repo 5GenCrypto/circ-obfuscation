c867cfad9116cdda9c35a53134b4d28ba117d75e
Alex J. Malozemoff <amaloz@galois.com>
2017-01-17 10:15:06 -0800

    add mode to output

--- info.sh
+++ info.sh
@@ -16,6 +16,8 @@ run () {
         flags="--rachel --symlen 16"
     fi
     name=$(basename $circuit | cut -d'.' -f1)
+    mode=$(basename $circuit | cut -d'.' -f2)
+    test $mode != "dsl" && test $mode != "c2a" && test $mode != "c2c" && mode=""
     $prog --get-kappa --scheme LIN --verbose $circuit $flags &>/tmp/results.txt
     ninputs=$(grep ninputs /tmp/results.txt | cut -d' ' -f 3)
     nconsts=$(grep nconsts /tmp/results.txt | cut -d' ' -f 3)
@@ -27,10 +29,10 @@ run () {
     lin=$(tail -1 /tmp/results.txt | cut -d' ' -f 3)
     $prog --get-kappa --scheme LZ --verbose $circuit $flags &>/tmp/results.txt
     lz=$(tail -1 /tmp/results.txt | cut -d' ' -f 3)
-    echo "$name, $ninputs, $nconsts, $noutputs, $size, $nmuls, $depth, $degree, $lin, $lz"
+    echo "$name, $mode, $ninputs, $nconsts, $noutputs, $size, $nmuls, $depth, $degree, $lin, $lz"
 }
 
-echo "nins, nkey, nouts, size, nmuls, depth, degree, lin.κ, lz.κ"
+echo "name, mode, nins, nkey, nouts, size, nmuls, depth, degree, lin.κ, lz.κ"
 for circuit in $(ls circuits/*.acirc); do
     run $circuit n
 done
