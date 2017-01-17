b8c8a5596a88782ed61d9e7998306ad46afac655
Alex J. Malozemoff <amaloz@galois.com>
2017-01-17 11:39:27 -0800

    latest and greatest circuits

--- info.sh
+++ info.sh
@@ -22,13 +22,13 @@ run () {
     ninputs=$(grep ninputs /tmp/results.txt | cut -d' ' -f 3)
     nconsts=$(grep nconsts /tmp/results.txt | cut -d' ' -f 3)
     noutputs=$(grep noutputs /tmp/results.txt | cut -d' ' -f 3)
-    size=$(grep size /tmp/results.txt | cut -d' ' -f 3)
-    nmuls=$(grep nmuls /tmp/results.txt | cut -d' ' -f 3)
-    depth=$(grep depth /tmp/results.txt | cut -d' ' -f 3)
+    size=$(grep "* size" /tmp/results.txt | cut -d' ' -f 3)
+    nmuls=$(grep " *nmuls" /tmp/results.txt | cut -d' ' -f 3)
+    depth=$(grep "* depth" /tmp/results.txt | cut -d' ' -f 3)
     degree=$(grep "* degree" /tmp/results.txt | cut -d' ' -f 3)
-    lin=$(tail -1 /tmp/results.txt | cut -d' ' -f 3)
+    lin=$(grep "κ = " /tmp/results.txt | cut -d' ' -f 3)
     $prog --get-kappa --scheme LZ --verbose $circuit $flags &>/tmp/results.txt
-    lz=$(tail -1 /tmp/results.txt | cut -d' ' -f 3)
+    lz=$(grep "κ =" /tmp/results.txt | cut -d' ' -f 3)
     echo "$name, $mode, $ninputs, $nconsts, $noutputs, $size, $nmuls, $depth, $degree, $lin, $lz"
 }
 
@@ -36,12 +36,9 @@ echo "name, mode, nins, nkey, nouts, size, nmuls, depth, degree, lin.κ, lz.κ"
 for circuit in $(ls circuits/*.acirc); do
     run $circuit n
 done
-for circuit in $(ls circuits/all-circuits/*.acirc); do
+for circuit in $(ls circuits/circuits/*.acirc); do
     run $circuit n
 done
-for circuit in $(ls circuits/all-circuits/ggm/*.acirc); do
-    run $circuit n
-done
-for circuit in $(ls circuits/all-circuits/ggm_rachel/*.acirc); do
+for circuit in $(ls circuits/all-circuits/rachel/*.acirc); do
     run $circuit y
 done
