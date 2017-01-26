1e97c828326cb1b9f350703d10cea8cde354d47c
Alex J. Malozemoff <amaloz@galois.com>
2017-01-26 14:42:18 -0800

    script for converting kappas.csv to latex

---
+++ kappas-to-latex.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo $1
+if [ x"$1" == x"-m" ]; then
+    usemodes=1
+else
+    usemodes=0
+fi
+
+while read input; do
+    line=$(echo $input | tr -d ' ')
+    name=$(echo $line | cut -d',' -f1)
+    name=$(perl -e "\$line = $name; \$line =~ s/_/\\\_/g; print \$line")
+    mode=$(echo $line | cut -d',' -f2)
+    ninputs=$(echo $line | cut -d',' -f3)
+    nconsts=$(echo $line | cut -d',' -f4)
+    nconsts=$(printf "%'.f" $nconsts)
+    nouts=$(echo $line | cut -d',' -f5)
+    size=$(echo $line | cut -d',' -f6)
+    size=$(printf "%'.f" $size)
+    nmuls=$(echo $line | cut -d',' -f7)
+    nmuls=$(printf "%'.f" $nmuls)
+    depth=$(echo $line | cut -d',' -f8)
+    degree=$(echo $line | cut -d',' -f9)
+    if [ ${#degree} -gt 6 ]; then
+        degree=$(printf %.2e $degree)
+    fi
+    if [ x"$name" == xname ]; then
+        continue
+    fi
+    if [ $usemodes -eq 1 ]; then
+        echo "\texttt{$name} && $mode && $ninputs && $nconsts && $nouts && $size && $nmuls && $depth && $degree \\\\"
+    else
+        echo "\texttt{$name} && $ninputs && $nconsts && $nouts && $size && $nmuls && $depth && $degree \\\\"
+    fi
+done
