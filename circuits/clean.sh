3eaeb93a3b9c3da94fa2e0ea44db581ea39dbab9
Alex J. Malozemoff <amaloz@galois.com>
2017-05-11 09:35:36 -0700

    add -f to rm

--- circuits/clean.sh
+++ circuits/clean.sh
@@ -2,8 +2,8 @@
 
 dir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 
-find "$dir" -name "*.ct" | xargs rm
-find "$dir" -name "*.obf" | xargs rm
-find "$dir" -name "*.ek" | xargs rm
-find "$dir" -name "*.sk" | xargs rm
+find "$dir" -name "*.ct" | xargs rm -f
+find "$dir" -name "*.obf" | xargs rm -f
+find "$dir" -name "*.ek" | xargs rm -f
+find "$dir" -name "*.sk" | xargs rm -f
 
